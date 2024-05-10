eb0a1edd5db9be04fe0e121037d3d0f7882d41b2
ARBUCHELI <radio_seattle@hotmail.com>
2024-05-10 10:36:21 +0300

    hello world - semicolons and whitespace

---
+++ HELLO WORLD/semicolons-and-whitespace/LanguageFacts.java
@@ -0,0 +1,11 @@
+public class LanguageFacts {
+    public static void main(String[] args) {
+      // Press enter or return on your keyboard after each semicolon!
+      
+      System.out.println("Java is a class-based language.");
+      System.out.println("Java classes have a 'main' method.");
+      System.out.println("Java statements end with a semicolon.");
+      System.out.println("Programming is the only good thing that I have in my life, and the only thing that saved me from falling in a big depression.  For that reason I love it.");
+    }
+  }
+  
